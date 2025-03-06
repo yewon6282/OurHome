23fd7b6d259d9f3ae424b34aa3526bbb32472844
yewonKim <ywkim@posbank.com>
2025-03-06 11:41:45 +0900

    design: small layout

--- app/src/main/java/com/yw_kim/ourhome/SplashActivity.kt
+++ app/src/main/java/com/yw_kim/ourhome/SplashActivity.kt
@@ -4,10 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 @Suppress("DEPRECATION")
 @SuppressLint("CustomSplashScreen")
